1a17d77428abc97509bc7bcbcf70564089f1297f
Renan Aba <124288351+renanabasilva@users.noreply.github.com>
2023-04-18 20:08:26 +0100

    ex_016

--- Aulas/ME_A13_14_Repetições/amb.js
+++ Aulas/ME_A13_14_Repetições/amb.js
@@ -1,5 +1,12 @@
-console.log('Vai começar...')
-for(var c = 1; c <= 5; c++){
-    console.log(c)
-}
-console.log('FIM!')
+// console.log('Vai começar...')
+// for(var c = 1; c <= 5; c++){
+//     console.log(c)
+// }
+// console.log('FIM!')
+
+    var ni = 1
+    var nf = 10
+    var np = 2
+    for (var c = ni; c <= nf; c += np){
+        console.log(c)
+    }
---
+++ Aulas/ME_A13_14_Repetições/ex016/javascript/script.js
@@ -0,0 +1,21 @@
+function contar(){
+    var inicio = document.getElementById('iini')
+    var fim = document.getElementById('ifim')
+    var passo = document.getElementById('ipas')
+    var ni = Number(inicio.value)
+    var nf = Number(fim.value)
+    var np = Number(passo.value)
+    if (inicio.value == '' || fim.value == ''){
+        alert('Impossível contar, insira um valor.')
+    }else {
+        if (np == 0){
+            alert('Passo inválido! Considerando PASSO 1')
+            np = 1
+        }
+        res.innerHTML = 'Contando: <br>'
+        for (var c = ni; c <= nf; c += np){
+            res.innerHTML += `${c} &#x1F449; `
+        }    
+        res.innerHTML += (`&#x1F3C1;`)        
+    }   
+}                

b7029030abd6ef57af62988a20af21192d4d2420
Renan Aba <124288351+renanabasilva@users.noreply.github.com>
2023-04-19 13:37:29 +0100

    aulas 15 e 16

---
+++ Aulas/MF_A15_Variáveis_Compostas/amb.js
@@ -0,0 +1,12 @@
+let num = [5, 8, 2, 9, 3]
+num[3] = 6     /* o 6 substitui o 9*/
+num.push(1)
+num.sort()
+console.log(num)
+console.log(num.length)
+let pos = num.indexOf(8)
+if (pos == -1){
+    console.log('O valor não foi encontrado.')
+} else {
+    console.log(`O valor está na posição ${pos}`)
+}

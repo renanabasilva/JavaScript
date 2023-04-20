aa2eee588474bb18c94d197ef7aea4cd0c52dcc5
Renan Aba <124288351+renanabasilva@users.noreply.github.com>
2023-04-20 13:02:14 +0100

    resumo aulas 15 e 16

--- Aulas/MF_A15_Variáveis_Compostas/amb.js
+++ Aulas/MF_A15_Variáveis_Compostas/amb.js
@@ -8,5 +8,5 @@ let pos = num.indexOf(8)
 if (pos == -1){
     console.log('O valor não foi encontrado.')
 } else {
-    console.log(`O valor está na posição ${pos}`)
+    console.log(`O valor está na posição ${pos} ${num.indexOf(5)}`)
 }

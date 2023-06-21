ef588d0c51062882611c737577c355d2335d0df4
Renan Aba <124288351+renanabasilva@users.noreply.github.com>
2023-06-21 18:02:15 +0100

    ex80-84

--- Exercicios_PY_JS/_testes.js
+++ Exercicios_PY_JS/_testes.js
@@ -1,3 +1,17 @@
-for (var c = 0; c < 51; c += 2){
-    console.log(`${c} `)
+const numeros = Array(6).fill(1).map((x, y) => x + y)
+var pares = []
+var impares = []
+for (let c of numeros){
+    console.log(c)
+    if (c % 2 == 0){
+        pares.push(c)
+        console.log(`${c} é par`)
+    } else if (c % 2 == 1){
+        impares.push(c)
+        console.log(`${c} é impar`)
+    }
 }
+
+console.log(numeros)
+console.log(pares)
+console.log(impares)

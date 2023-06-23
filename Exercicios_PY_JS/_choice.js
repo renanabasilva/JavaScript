4ba0f136d05a12f0a381ec01d4d648b4deb181de
Renan Aba <124288351+renanabasilva@users.noreply.github.com>
2023-06-23 19:57:43 +0100

    ex091-095

--- Exercicios_PY_JS/_choice.js
+++ Exercicios_PY_JS/_choice.js
@@ -1,3 +1,5 @@
+const numeros = Array(6).fill(1).map((x, y) => x + y)
+
 function choice(array) {
     let index = Math.floor(array.length * Math.random())
     return array[index]
---
+++ Exercicios_PY_JS/_date.js
@@ -0,0 +1,4 @@
+let date = new Date()
+let ano = date.getFullYear()
+
+let ano2 = new Date().getFullYear()

da7f12a5006e2bd0a9bb653b5ff80aabe2315043
Renan Aba <124288351+renanabasilva@users.noreply.github.com>
2023-07-01 14:24:03 +0100

    finalização

--- Alura/#1-JS-web-paginas-dinamicas/main.js
+++ Alura/#1-JS-web-paginas-dinamicas/main.js
@@ -1,11 +1,17 @@
+function tocaSom(seletorAudio){
+    const elemento = document.querySelector(seletorAudio);
+    
+    if (elemento && elemento.localName === 'audio'){
+        elemento.play();
+    } else {
+        console.log('Elemento não encontrado ou seletor inválido.');        
+    }
+}
+
 const listaDeTeclas = document.querySelectorAll('.tecla'); 
 let contador = 0;
 
-function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();
-}
-
-while(contador < listaDeTeclas.length){
+for( let contador = 0; contador < listaDeTeclas.length; contador++){
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
@@ -14,5 +20,12 @@ while(contador < listaDeTeclas.length){
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
-    contador++;
+    tecla.onkeydown = function (evento) {
+        if (evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa');            
+        }
+    }
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 }

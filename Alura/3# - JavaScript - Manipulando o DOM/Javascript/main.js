53d92449f6d793a44aec8e5680178a4d2edfdbf6
Renan Aba <124288351+renanabasilva@users.noreply.github.com>
2023-07-06 20:37:04 +0100

    A3

---
+++ Alura/3# - JavaScript - Manipulando o DOM/Javascript/main.js
@@ -0,0 +1,10 @@
+const robotron = document.querySelector("#robotron")
+
+robotron.addEventListener("click", (evento) => {
+    console.log(evento)
+})
+
+function dizOi(){
+    console.log("oi")
+}
+

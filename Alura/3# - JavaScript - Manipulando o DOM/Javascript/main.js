fbd1d1dfcb72bd9d1d0b9f37d2b75bf7514b9127
Renan Aba <124288351+renanabasilva@users.noreply.github.com>
2023-07-11 20:27:11 +0100

    .

--- Alura/3# - JavaScript - Manipulando o DOM/Javascript/main.js
+++ Alura/3# - JavaScript - Manipulando o DOM/Javascript/main.js
@@ -1,4 +1,7 @@
 const robotron = document.querySelector("#robotron")
+const subtrair = document.querySelector("#subtrair");
+const somar = document.querySelector("#somar");
+const braco = document.querySelector("#braco");
 
 robotron.addEventListener("click", (evento) => {
     console.log(evento)
@@ -8,3 +11,14 @@ function dizOi(){
     console.log("oi")
 }
 
+somar.addEventListener("click", (evento) =>{manipulaDados("somar")})
+
+subtrair.addEventListener("click", (evento) =>{manipulaDados("subtrair")})
+
+function manipulaDados(operacao){
+    if (operacao === "subtrair"){
+        braco.value = parseInt(braco.value) - 1
+    } else {
+        braco.value = parseInt(braco.value) + 1
+    }
+}

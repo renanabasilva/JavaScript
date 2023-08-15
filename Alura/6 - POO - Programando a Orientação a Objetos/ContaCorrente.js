f81625fddc2d9b91b2f4e46a13b7d9b9cc455f50
Renan Aba <124288351+renanabasilva@users.noreply.github.com>
2023-08-15 20:19:07 +0100

    estudo

---
+++ Alura/6 - POO - Programando a Orientação a Objetos/ContaCorrente.js
@@ -0,0 +1,14 @@
+export class ContaCorrente {
+  agencia;
+  #saldo = 0;
+  sacar(valor) {
+    if (this.#saldo < valor) return
+    this.#saldo -= valor;
+    return valor;
+  }
+
+  depositar(valor) {
+    if (valor <= 0) return
+    this.#saldo += valor;
+  }
+}

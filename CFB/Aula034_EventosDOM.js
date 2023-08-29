06c8034cf603586665b138832d89ae2ce708b1f4
Renan Aba <124288351+renanabasilva@users.noreply.github.com>
2023-08-29 20:21:36 +0100

    Estudo

---
+++ CFB/Aula034_EventosDOM.js
@@ -0,0 +1,33 @@
+// const c1 = document.querySelector("#c1")
+
+// const msg = () => {
+//   alert("clicou")
+// }
+
+// c1.addEventListener("click", ()=>{
+//   alert("clicou")
+// })
+
+// c1.addEventListener("click", msg)
+
+// c1.addEventListener("click", ()=>{
+//   msg()
+// })
+
+// c1.addEventListener("click", (evento)=>{
+//   const elemento = evento.target
+//   elemento.classList.add("destaque")
+// })
+
+// ---------------------
+
+const cursos = [...document.querySelectorAll(".curso")]
+
+cursos.map((elemento) => {
+  elemento.addEventListener("click", (evento) =>{
+    const elemento = evento.target
+    elemento.classList.add("destaque")
+    console.log(elemento.id)
+    console.log(elemento.innerHTML)
+  })
+})

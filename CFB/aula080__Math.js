9b40b220dc353bba74ff16f993db53f56c6d3d39
Renan Aba <124288351+renanabasilva@users.noreply.github.com>
2023-09-29 15:50:10 +0100

    estudo

---
+++ CFB/aula080__Math.js
@@ -0,0 +1,9 @@
+const mat = document.querySelector("#mat")
+
+let zerocem = Math.round(Math.random() * 100)
+let umEDez = Math.floor(Math.random()*10)+1
+let raiz = Math.sqrt(144)
+let potencia = Math.pow(10, 3)
+
+
+mat.innerHTML = umEDez

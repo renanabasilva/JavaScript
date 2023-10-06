8b4acd8e691f3eb95c39f11e6a226082586ece94
Renan Aba <124288351+renanabasilva@users.noreply.github.com>
2023-10-06 16:02:11 +0100

    estudo

--- CFB/aula088__CaixasDeDialogo.js
+++ CFB/aula088__CaixasDeDialogo.js
@@ -3,13 +3,23 @@ const btn_confirm = document.querySelector("#btn_confirm")
 const btn_prompt = document.querySelector("#btn_prompt")
 
 btn_alert.addEventListener("click",(evt) =>{
-  
+  alert('Olá, como você está?')
 })
 
 btn_confirm.addEventListener("click",(evt) =>{
-  
+  const ret = window.confirm('Você está pronto?')
+  if(ret){
+    console.log('Botão Confirm pressionado')
+  } else {
+    console.log('Botão Cancelar pressionado')
+  }
 })
 
 btn_prompt.addEventListener("click",(evt) =>{
-  
+  const nome = prompt('Digite seu nome:','texto padrão')
+  if (nome === null){
+    console.log('Botão cancelar pressionado.')
+  } else {
+    console.log(`Nome digitado: ${nome}`)
+  }
 })
---
+++ CFB/aula089__ImpressaoEPDF.js
@@ -0,0 +1,23 @@
+const btn_imp = document.querySelector("#btn_imp")
+
+btn_imp.addEventListener("click",(evt) =>{
+  const conteudo = document.querySelector("#tabela").innerHTML
+
+  let estilo = "<style>"
+  estilo += "table {width: 100%; font: 25px Calibri;}"
+  estilo += "table, th, td {border: solid 2px #888; border-collapse: collapse;"
+  estilo += "padding: 4px 8px; text-align: center;}"
+  estilo += "</style>"
+
+  const win = window.open('', '', 'height=700, width=700')
+
+  win.document.write('<html><head>')
+  win.document.write('<title>Curso</title>')
+  win.document.write(estilo)
+  win.document.write('</head><body>')
+  win.document.write(conteudo)
+  win.document.write('</body></html>')
+
+  win.print()
+  // win.close()
+})

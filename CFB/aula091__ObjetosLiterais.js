2921c176cef065493c8d84b75ad60f5bf00b7143
Renan Aba <124288351+renanabasilva@users.noreply.github.com>
2023-10-09 19:35:02 +0100

    Update aula091__ObjetosLiterais.js

--- CFB/aula091__ObjetosLiterais.js
+++ CFB/aula091__ObjetosLiterais.js
@@ -1,10 +1,10 @@
 const objetos = document.querySelector("#objetos")
 
 const computador = {
-  cpu:'i9',
-  ram:'64gb',
-  hd:'2tb',
-  info: function(){
+  cpu: 'i9',
+  ram: '64gb',
+  hd: '2tb',
+  info: function () {
     console.log(`CPU: ${this.cpu}`)
     console.log(`RAM: ${this.ram}`)
     console.log(`HD: ${this.hd}`)
@@ -13,25 +13,44 @@ const computador = {
 
 computador['monitor'] = '22 pol'
 computador.placaVideo = 'RTX'
-console.log(computador)
+
+const c1 = Object.assign({}, computador)
+
+delete(computador.hd)
+c1.info()
+computador.info()
+
+const c2 = Object.create(computador)
+c2.cpu = 'i9'
+c2.ram = '16gb'
+c2.hd = '1tb'
+
+
+const o1 = {obj1:'1'}
+const o2 = {obj2:'2'}
+const o3 = {obj3:'3'}
+const o4 = Object.assign(o1, o2, o3)
+
+// console.log(o4)
+// console.log(computador)
 
 const computadores = [
   {
-    cpu:'i9',
-    ram:'64gb',
-    hd:'2tb',
+    cpu: 'i9',
+    ram: '64gb',
+    hd: '2tb',
   },
   {
-    cpu:'i7',
-    ram:'32gb',
-    hd:'2tb',
-  
+    cpu: 'i7',
+    ram: '32gb',
+    hd: '2tb',
+
   },
   {
-    cpu:'i5',
-    ram:'16gb',
-    hd:'1tb',
-  
+    cpu: 'i5',
+    ram: '16gb',
+    hd: '1tb',
+
   }
 ]
 
@@ -39,7 +58,7 @@ const computadores = [
 // console.log(computador)
 // objetos.innerHTML = JSON.stringify(computadores)
 
-computadores.forEach((c)=>{
+computadores.forEach((c) => {
   const div = document.createElement('div')
   div.innerHTML = c.cpu + '<br>' + c.ram + '<br>' + c.hd
   div.setAttribute('class', 'computador')

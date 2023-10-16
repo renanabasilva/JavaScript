2a023a22815cc738aa59ffbd1563b0b5eb732b01
Renan Aba <124288351+renanabasilva@users.noreply.github.com>
2023-10-16 15:06:36 +0100

    estudo

--- CFB/aula105__RegEx.js
+++ CFB/aula105__RegEx.js
@@ -58,19 +58,78 @@ console.log(nome.match(/\bRenan\b/ig))    // Verifica tanto inicio quanto final 
 console.log(nome.match(/\bab/ig))    // Com o '\b' ANTES, mostra palavras que começam com o termo
 console.log(nome.match(/lva\b/ig))    // Com o '\b' DEPOIS, mostra palavras que terminam com o termo
 
+
+console.log('---- Shorthands ----')
+
+let num = '0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10'
+console.log(num.match(/[0-9]/))
+console.log(num.match(/\d/ig))    // Shorthand para [0-9]
+console.log(nome.match(/[a-zA-Z0-9_]/))
+console.log(nome.match(/\w/ig))   // Shorthand para caracteres e números
+console.log(nome.match(/[\r\n\t\f\v]/ig))
+// r = carriage return (impressora)
+// n = nova linha (return)
+// t = tab
+// f = form-feed
+// v = vertical tab
+console.log(nome.match(/\s/ig))   // Shorthand para espaços em branco
+// s = Qualquer espaço em branco
+
+// [:digit:] digits
+// [:alpha:] letters
+// [:lower:] lowercase letters
+// [:upper:] uppercase letters
+// [:alnum:] letters and numbers
+// [:punct:] punctuation
+// [:graph:] letters, numbers, and punctuation
+// [:space:] space characters (i.e. \s)
+// [:blank:] space and tab (but not new line)
+
+// pattern: /\(\d\d\)\s\d\d\d\d\d-\d\d\d\d/
+// string:  (86) 95262-7297
+// matches: ^^^^^^^^^^^^^^^
+// pattern: /\w\w\w\w\w@\w\w\.com/
+// string:  ax_a5@5x.com
+// matches: ^^^^^^^^^^^^
+
+// pattern: /[a\-o]/gi
+// string:  cachorro-quente.
+// matches:  ^  ^  ^^
+// Para usar a forma literal de um acento, pode-se usar o \ antes
+
+// console.log('---- Para usar \\\\ \' \" ----')
+
 console.log('---- Quantificadores ----')
+console.log('---- + ----')
 let nome1 = new String('Renaaaan Abaaa daa Siiiiilvaaa 1992')
 console.log(nome1.match(/a/ig))
 console.log(nome1.match(/a+|i+/ig))
-// O uso do '+', agrupa os caracteres que se repetem seguidamente
+// O uso do '+' = uma ou mais ocorrências
+
+// pattern: /go+l+/gi
+// string:  Goolllll da Alemanha!!!
+// matches: ^^^^^^^^
+
+console.log('---- * ----')
 
 let numeros = '0 ,1, 10, 100, 1001, 1000, 102115004'
 console.log(numeros.match(/10/ig))
-console.log(numeros.match(/10+/ig))
 console.log(numeros.match(/10*/ig))
-// O uso do '*', 
+// O uso do '*' = zero ou mais ocorrências
+// Corresponde a expressão que o precede repetida 0 ou mais vezes. Equivalente a {0,}
+// Por exemplo, /bo*/ acha uma correspondência para 'boooo' em "Scoob doo" e 'b' em "A bird warbled", mas nenhuma em "A goat grunted".
+
+console.log('---- ? ----')
 
 console.log(numeros.match(/10?/ig))
-// O uso da '?', 
+// O uso da '?' = zero ou uma ocorrência
 
-// console.log('---- Para usar \\\\ \' \" ----')
+//  Validação de CPF
+// pattern: /\d{3}\.?\d{3}\.?\d{3}-?\d{2}/
+// string:  825.531.760-07
+// matches: ^^^^^^^^^^^^^^
+// string:  18646661024
+// matches: ^^^^^^^^^^^
+
+
+console.log('---- Âncoras ----')

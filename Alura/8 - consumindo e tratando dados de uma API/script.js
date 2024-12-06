ff3d9cd12c8fba083e07bcbc91bcb5bad4fac000
Renan Aba <124288351+renanabasilva@users.noreply.github.com>
2024-12-06 19:52:27 +0000

    aula 02 - estudo

---
+++ Alura/8 - consumindo e tratando dados de uma API/script.js
@@ -0,0 +1,31 @@
+const containerVideos = document.querySelector(".videos__container");
+
+const api = fetch("http://localhost:3000/videos")
+  .then(res => res.json())
+  .then(videos =>
+    videos.forEach(video => {
+      containerVideos.innerHTML += `
+        <li class="videos__item">
+          <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+          <div class="descricao-video">
+            <img class="img-canal" src="${video.imagem} alt="logo do canal""/>
+            <h3 class="titulo-video">${video.titulo}</h3>
+            <p class="titulo-canal">${video.descricao}</p>
+          </div>
+        </li>
+      `;
+    })
+  )
+  .catch(error => {
+    containerVideos.innerHTML = `<p>Erro ao carregar os vídeos: ${error}</p>`;
+  })
+
+
+//   let promise = new Promise((resolve, reject) => {
+//     // código assíncrono aqui
+//     if (/* tudo ocorreu bem */) {
+//         resolve("funcionou!");
+//     } else {
+//         reject("houve um erro");
+//     }
+// });

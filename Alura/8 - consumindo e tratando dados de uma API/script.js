04e394e237e4e78dbfd877274d3c3da891333b4e
Renan Aba <124288351+renanabasilva@users.noreply.github.com>
2024-12-10 14:06:00 +0000

    aula 04 - estudo

--- Alura/8 - consumindo e tratando dados de uma API/script.js
+++ Alura/8 - consumindo e tratando dados de uma API/script.js
@@ -16,6 +16,7 @@ async function buscarEMostrarVideos() {
         <img class="img-canal" src="${video.imagem} alt="logo do canal""/>
         <h3 class="titulo-video">${video.titulo}</h3>
         <p class="titulo-canal">${video.descricao}</p>
+        <p class="categoria" hidden>${video.categoria}</p>
         </div>
         </li>
       `;
@@ -29,6 +30,51 @@ async function buscarEMostrarVideos() {
 }
 
 buscarEMostrarVideos();
+
+const barraDePesquisa = document.querySelector(".pesquisar__input");
+
+barraDePesquisa.addEventListener("input", filtrarPesquisa);
+
+function filtrarPesquisa() {
+  const videos = document.querySelectorAll(".videos__item");
+
+  if(barraDePesquisa.value != ""){
+    for(let video of videos){
+      let titulo = video.querySelector(".titulo-video").textContent.toLowerCase();
+      let valorFiltro = barraDePesquisa.value.toLowerCase();
+
+      if(!titulo.includes(valorFiltro)){
+        video.style.display = "none";
+      } else {
+        video.style.display = "block";
+      }
+    }
+  } else {
+     videos.style.display = "block";
+  }
+}
+
+const botaoCategoria = document.querySelectorAll(".superior__item");
+
+botaoCategoria.forEach((botao) => {
+  let nomeCategoria = botao.getAttribute("name");
+  botao.addEventListener("click", () => filtrarPorCategoria(nomeCategoria));
+})
+
+function filtrarPorCategoria(filtro) {
+  const videos = document.querySelectorAll(".videos__item");
+
+  for(let video of videos){
+    let categoria = video.querySelector(".categoria").textContent.toLowerCase();
+    let valorFiltro = filtro.toLowerCase();
+
+    if(!categoria.includes(valorFiltro) && valorFiltro != "tudo"){
+      video.style.display = "none";
+    } else {
+      video.style.display = "block";
+    }
+  }
+}
 
 // const api = fetch("http://localhost:3000/videos")
 // .then(res => res.json())
